f25f3da22c28e93fed0aaad095430f1f7b0e199e
shepovnik <poli.shep@gmail.com>
2023-03-03 16:28:08 +0300

    Lr2

--- main.js
+++ main.js
@@ -126,3 +126,84 @@ let m = +prompt( "введите номер месяца от 1 до 12" )
         console.log("месяц", m, "=> 4 квартал")
     }
 }
+
+//----------------------------------------------------------1-1
+function first1()
+{
+let value = +prompt( "введите скорость (число)" )
+let measure = prompt( "введите направление преобразования: м/с или  км/ч" )   
+let x = function convertSpeed(value, measure)
+{ 
+    if (measure == "км/ч")
+    { 
+        return value * 1000 / 3600
+    }
+    else if (measure == "м/с")
+    {
+        return value * 3600 / 1000     
+    }
+}
+console.log("convertSpeed (", value, ", 'toKMH') -> " + x(value, measure), " '", measure, "'")
+}
+//----------------------------------------------------------2-1
+function second1()
+{
+    let abs = +prompt( "введите число, для которого нужно найти модуль" )
+
+let xx = function absValue (abs){
+    if (abs < 0)
+    {
+        return -abs
+    }
+    else
+    {
+        return abs
+    }
+}
+console.log("absValue(", abs,") ->", xx(abs))
+}
+
+//----------------------------------------------------------3-1
+function three1()
+{
+    const student  = { group: 201, last_name: "Иванов", first_name: "Иван" };
+
+    for (let property in student ) {
+    console.log("список свойств:", property)
+}
+    console.log("Студент", student.first_name, student.last_name, "учится в", student.group, "группе")
+    
+}
+
+//----------------------------------------------------------4-1
+
+function four1()
+{
+    let min = +prompt( "введите минимальное число диапазона" )
+    let max = +prompt( "введите максимальное число диапазона" )
+
+    let xx = function randomNumber (min, max)
+    {
+        let rand = min + Math.random() * (max + 1 - min);
+        return Math.floor(rand);
+    }
+    console.log("randomNumber(", min,",", max,") ->", xx(min, max))
+}
+
+//----------------------------------------------------------5-1
+
+function sampleArray()
+{
+let arr = [0,1,2,3,4,5,6,7,8,9,10]
+let arr1 = []
+let ch = +prompt( "введите количество выводимых значений из массива" )
+let i = 0
+let rand
+while (i != ch)
+{
+    rand = Math.floor(Math.random() * arr.length);
+    arr1.push(rand)
+    i++
+}
+console.log("sampleArray(", arr, ",", ch,") ->", arr1);
+}
